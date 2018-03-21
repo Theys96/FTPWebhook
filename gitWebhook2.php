b8119661aa25802109d509127974ff4973623dfe
Theys96 <accounts@thijshavinga.nl>
2018-03-21 12:36:31 +0100

    Updated webhook, pretty much works now

--- gitWebhook2.php
+++ gitWebhook2.php
@@ -14,9 +14,14 @@ class ftp {
     }
 }
 
-function isfile($repo, $before, $file){
-   $headers = get_headers("https://raw.githubusercontent.com/" . $repo . "/" . $before . "/" . $file);
-   return stripos($headers[0],"200 OK")?true:false;
+function verifyDir($conn, $add) {
+	$dir = explode("/", $add);
+	array_pop($dir);
+	$makedir = ".";
+	foreach ($dir as $d) {
+		$makedir .= "/" . $d;
+		$conn->ftp_mkdir($makedir);
+	}
 }
 
 $configs = array();
@@ -43,55 +48,26 @@ $conn->ftp_chdir($repConfig[3]);
 
 for ($i = 0; $i < count($payload['commits']); $i++) {
 	$commit = $payload['commits'][$i];
-	echo "Commit: \"" . $commit['message'] . "\".\n";
+	echo "\nCommit: \"" . $commit['message'] . "\".\n";
 	echo "ID: " . $commit['id'] . "\n";
 	echo "Base URL: " . "https://raw.githubusercontent.com/" . $repConfig[4] . "/" . $commit['id'] . "/\n";
 	foreach ($commit['added'] as $add) {
+		verifyDir($conn, $add);
 		$conn->ftp_put($add, "https://raw.githubusercontent.com/" . $repConfig[4] . "/" . $commit['id'] . "/" . $add, FTP_BINARY);
 		echo "Added " . $add . "\n";
 	}
-	foreach ($commit['deleted'] as $del) {
+	foreach ($commit['removed'] as $del) {
 		$conn->ftp_delete($del);
 		echo "Deleted " . $del . "\n";
 	}
 	foreach ($commit['modified'] as $mod) {
+		verifyDir($conn, $add);
 		$conn->ftp_put($mod, "https://raw.githubusercontent.com/" . $repConfig[4] . "/" . $commit['id'] . "/" . $mod, FTP_BINARY);
 		echo "Modified " . $mod . "\n";
 	}
 }
 
-$conn->ftp_close();
-
-/* Backup
-foreach(array('added', 'removed', 'modified') as $type) {
-	foreach ($filechanges[$type] as $file) {
-		$conn->ftp_rename($file, "git_archives/" . $id . "/" . str_replace('/','_',$file));
-	}
-}
-
-/* Actual execution
-sort($dirchanges['added']);
-foreach ($dirchanges['added'] as $dir) {
-	$conn->ftp_mkdir($file);
-}
-
-foreach ($filechanges['removed'] as $file) {
-	$conn->ftp_delete($file, $file);
-}
-
-foreach(array('added', 'modified') as $type) {
-	foreach ($filechanges[$type] as $file) {
-		$conn->ftp_put($file, "https://raw.githubusercontent.com/" . $repConfig[4] . "/" . $longid . "/" . $file, FTP_BINARY);
-		echo "Fetching https://raw.githubusercontent.com/" . $repConfig[4] . "/" . $longid . "/" . $file . "\n";
-	}
-}
-
-rsort($dirchanges['removed']);
-foreach ($dirchanges['removed'] as $dir) {
-	$conn->ftp_rmdir($dir);
-}
-
+/* TODO HERE: Remove any empty directory */
 
 $conn->ftp_close();
-*/
 ?>
